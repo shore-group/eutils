0cf523fc9c028754c6c56c3299888a4813772155
Zach Smith <x.coder.zach@gmail.com>
2018-07-05 10:08:09 -0400

    Update bulk download script

--- tryout.py
+++ tryout.py
@@ -1,22 +1,31 @@
-from eutils import Client
-
 api_key = 'a1894416aafef699594319d11fc1a565db08'
 
 client = Client(api_key=api_key)
 
-esr = client.esearch(db='pubmed', term='("2013/01/01"[Date - Publication] : "3000"[Date - Publication])')
+parts = [str(d).split('-') for d in pandas.date_range('2015-04-01', '2018-07-01', freq='M')]
+months = [part[0] + '/' + part[1] for part in parts]
 
 per_page = 5000
 
-for i in range(0, esr.count, per_page):
-    with open('dump/pubmed-{start}-{end}.xml'.format(start=i, end=i + per_page), 'wb') as f:
-        result = client.efetch({
-          'db': 'pubmed',
-          'WebEnv': esr.webenv,
-          'query_key': esr.querykey,
-          'retmax': per_page,
-          'retstart': i
-        })
-
-        f.write(result)
-
+for month in months:
+    esr = client.esearch(db='pubmed', term='("{month}"[Date - Publication])'.format(month=month))
+    print('Starting {month} with {count} total articles'.format(month=month, count=esr.count))
+    for i in range(0, esr.count, per_page):
+        filename = 'dump/pubmed-{month}-{start}-{end}.xml'.format(month=month.replace('/', '-'), start=i, end=i + per_page)
+        with open(filename, 'wb') as f:
+            for i in range(0, 4):
+                try:
+                    result = client.efetch({
+                      'db': 'pubmed',
+                      'WebEnv': esr.webenv,
+                      'query_key': esr.querykey,
+                      'retmax': per_page,
+                      'retstart': i
+                    })
+                    f.write(result)
+                    print(filename)
+                    break
+                except:
+                    print("\n")
+                    print("Failed " + i + "times")
+                    time.sleep(pow(2, i + 3))
